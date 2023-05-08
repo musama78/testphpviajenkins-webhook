eb9500aee5d13d0626016152d5ded71ef389e1d8
musama78 <116833620+musama78@users.noreply.github.com>
2023-05-08 12:46:57 +0500

    index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+ <head>
+  <title>Hello | Docker class</title>
+</head>
+<body>
+    <div class="container">
+      <?php echo "<h1>Hi! I'm Ranjit</h1>"; ?>
+    	<?php echo "<h3>This is Docker Compose Class!</h3>"; ?>
+    	<?php echo "<h3>This is Docker Compose Class!</h3>"; ?>
+     	<?php echo "<h3>This is Docker Compose Class!</h3>"; ?>
+    	<?php echo "<h3>hello!</h3>"; ?>
+    	<?php echo "<h3>hello!</h3>"; ?>
+	    
+    <?php
+
+</body>
+</html>

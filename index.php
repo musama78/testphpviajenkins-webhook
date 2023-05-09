4f1ee9492c6842645be33490cbfa18e9d339eddd
musama78 <116833620+musama78@users.noreply.github.com>
2023-05-09 12:02:08 +0500

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 </head>
 <body>
 
-    <?php echo "<h1>Hi! I'm Usama, This is SCM auto pull + image push... 11:52</h1>"; ?>
+    <?php echo "<h1>Hi! I'm Usama, This is SCM auto pull + image push... 12.02</h1>"; ?>
     <?php echo "<h3>This is Docker Compose Class!</h3>"; ?>
 
 </body>
